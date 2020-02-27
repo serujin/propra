d860972e922dddfe41cb00cc91ea163ea0e75969
selcox <sergio.munoz.lillo@gmail.com>
2020-02-27 20:08:52 +0100

    All works, but ex1 need a revision, ex2 need to change void to Vector, ex 3 need to be purified

--- src/entregas/poo1/poo1Ejercicio1/Conjunto.java
+++ src/entregas/poo1/poo1Ejercicio1/Conjunto.java
@@ -14,75 +14,48 @@ public class Conjunto {
 			elements.remove(element);
 		}
 	}
-	public void showUnion(Conjunto comparation) { //Works perfectly, need GUI
+	public Conjunto union(Conjunto comparation) { //Works perfectly, need GUI
 		Conjunto aux = new Conjunto();
+		aux.elements.addAll(comparation.elements);
 		for(Integer element : elements) {
 			aux.addElement(element);
 		}
-		for(Integer element : comparation.elements) {
-			aux.addElement(element);
-		}
-		aux.showElements();
+		return aux;
 	}
-	public void showIntersect(Conjunto comparation) { //Need to get FIXED
+	public Conjunto intersect(Conjunto comparation) { //Need to get FIXED
 		Conjunto aux = new Conjunto();
-
 		for(Integer compElement : comparation.elements) {
 			if(this.checkExist(compElement)) {
 				aux.addElement(compElement);
 			}
 		}
-		aux.showElements();
+		return aux;
 	}
-	public void showDifference(Conjunto comparation) { //Need to get FIXED
+	public Conjunto difference(Conjunto comparation) { //Need to get FIXED
 		Conjunto aux = new Conjunto();
-		Conjunto aux2 = new Conjunto();
-		for(Integer element : elements) {
-			aux.addElement(element);
-		}
-		for(Integer compElement : comparation.elements) {
-			if(this.checkExist(compElement)) {
-				aux2.addElement(compElement);
-			}
-		}
-		for(Integer delElement : aux2.elements) {
+		aux.elements.addAll(elements);
+		for(Integer delElement : comparation.elements) {
 			aux.delElement(delElement);
 		}
-		aux.showElements();
+		return aux;
 	}
 	public boolean checkExist(Integer element) { //Works perfectly
-		for(Integer listElement : elements) {
-			if(listElement.equals(element)) {
-				return true;
-			}
-		}
-		return false;
+		return elements.contains(element);
 	}
 	public boolean checkSubElement(Conjunto comparation) {
-		for(Integer element : elements) {
-			if(!comparation.checkExist(element)) {
-				return false;
-			}
-		}
-		return true;
+		return elements.containsAll(comparation.elements);
 	}
 	public boolean checkEquals(Conjunto comparation) {
-		if(elements.size()!=comparation.elements.size()) {
-			return false;
-		}
-		for(Integer element : elements) {
-			if(!comparation.checkExist(element)) {
-				return false;
+		return elements.size()==comparation.elements.size() && checkSubElement(comparation);
+	}		
+	public void showElements() {
+		if(elements.size()==0) {
+			System.out.println("El conjunto está vacío");
+		} else {
+			for(Integer element : elements) {
+				System.out.print("el"+element+" ");
 			}
-		}
-		return true;
-	}	
-	public void showElements() { //Works perfectly, need GUI
-		System.out.print(elements+"\n");
-	}	
-	public void showFormatElements() {
-		for(Integer element : elements) {
-			System.out.print(" el"+element);
+			System.out.println("");
 		}
 	}
 }
--- src/entregas/poo1/poo1Ejercicio1/EjercicioPoo1.java
+++ src/entregas/poo1/poo1Ejercicio1/EjercicioPoo1.java
@@ -3,117 +3,54 @@ package entregas.poo1.poo1Ejercicio1;
 public class EjercicioPoo1 {
 	static Conjunto a = new Conjunto();
 	static Conjunto b = new Conjunto();
-	static Conjunto c = new Conjunto();
-	static Conjunto d = new Conjunto();
-	static Conjunto e = new Conjunto();
-	static Conjunto f = new Conjunto();
 	public static void main(String[] args) {
-		initConjuntos();
-		showConjuntos();
-		showUnions();
-		showIntersections();
-		showDifferences();
-		showExists();
-		showSubElements();
-		showEquals();
-		showFormatElements();
+		System.out.println("Casos de prueba addElements");
+		addElementsTestCases();
+		a.showElements();
+		b.showElements();
+		System.out.println("\nCasos de prueba delElements");
+		delElementsTestCases();
+		System.out.println("\nCasos de prueba union");
+		unionTestCases();
+		System.out.println("\nCasos de prueba intersect");
+		intersectTestCases();
+		System.out.println("\nCasos de prueba difference");
+		differenceTestCases();
 	}
-	public static void initConjuntos() {
+	public static void addElementsTestCases() {
 		for(int i=1,j=4;i<7;i++,j++) {
 			a.addElement(i);
 			b.addElement(j);
 		}
-		for(int i=1,j=6;i<4;i++,j--) {
-			c.addElement(i);
-			d.addElement(j);
-		}
-		f.addElement(6);
-		f.addElement(9);
-		f.addElement(4);
-		f.addElement(5);
-		f.addElement(8);
-		f.addElement(7);
-		
-		e.addElement(3);
-		e.addElement(5);
-		e.addElement(4);
-		e.addElement(1);
-		e.addElement(6);
-		e.addElement(2);
 	}
-	public static void showConjuntos() {
-		System.out.print("Conjunto A: ");
+	public static void delElementsTestCases() {
+		a.delElement(30);
 		a.showElements();
-		System.out.print("Conjunto B: ");
-		b.showElements();
-		System.out.print("Conjunto C: ");
-		c.showElements();
-		System.out.print("Conjunto D: ");
-		d.showElements();
-		System.out.print("Conjunto E: ");
-		e.showElements();
-		System.out.print("Conjunto F: ");
-		f.showElements();
+		a.delElement(2);
+		a.showElements();
+		addElementsTestCases(); //Just to restart the elements
 	}
-	public static void showUnions() {
-		System.out.print("\nA union B: ");
-		a.showUnion(b);
-		System.out.print("A union C: ");
-		a.showUnion(c);
-		System.out.print("B union C: ");
-		b.showUnion(c);
+	public static void unionTestCases() {
+		Conjunto aux = new Conjunto();
+		aux = a.union(b);
+		aux.showElements();
+		aux = a.union(a);
+		aux.showElements();
 	}
-	public static void showIntersections() {
-		System.out.print("\nA intersección B: ");
-		a.showIntersect(b);
-		System.out.print("A intersección C: ");
-		a.showIntersect(c);
-		System.out.print("B intersección C: ");
-		b.showIntersect(c);
+	public static void intersectTestCases() {
+		Conjunto aux = new Conjunto();
+		aux = a.intersect(b);
+		aux.showElements();
+		aux = a.intersect(a);
+		aux.showElements();
 	}
-	public static void showDifferences() {
-		System.out.print("\nA - B: ");
-		a.showDifference(b);
-		System.out.print("A - C: ");
-		a.showDifference(c);
-		System.out.print("B - C: ");
-		b.showDifference(c);
+	public static void differenceTestCases() {
+		Conjunto aux = new Conjunto();
+		aux = a.difference(b);
+		aux.showElements();
+		aux = a.difference(a);
+		aux.showElements();
 	}
-	public static void showExists() {
-		System.out.print("\nA contiene 8: "+a.checkExist(8));
-		System.out.print("\nB contiene 4: "+b.checkExist(4));
-		System.out.print("\nC contiene 6: "+c.checkExist(6));
-		System.out.print("\nA contiene 2: "+a.checkExist(2));
-		System.out.print("\nB contiene 9: "+b.checkExist(9));
-		System.out.print("\nC contiene 1: "+c.checkExist(1));
-	}
-	public static void showSubElements() {
-		System.out.print("\n\nD es un subconjunto de B: "+d.checkSubElement(b));
-		System.out.print("\nA es un subconjunto de C: "+a.checkSubElement(c));
-		System.out.print("\nB es un subconjunto de A: "+b.checkSubElement(a));
-		System.out.print("\nD es un subconjunto de C: "+d.checkSubElement(c));
-		System.out.print("\nC es un subconjunto de A: "+c.checkSubElement(a));
-		System.out.print("\nD es un subconjunto de A: "+d.checkSubElement(a));
-	}
-	public static void showEquals() {
-		System.out.print("\n\nA = C: "+a.checkEquals(c));
-		System.out.print("\nE = A: "+e.checkEquals(a));
-		System.out.print("\nB = D: "+b.checkEquals(d));
-		System.out.print("\nF = B: "+f.checkEquals(b));
-	}
-	public static void showFormatElements() {
-		System.out.print("\n\nConjunto A con formato especial: ");
-		a.showFormatElements();
-		System.out.print("\nConjunto B con formato especial: ");
-		b.showFormatElements();
-		System.out.print("\nConjunto C con formato especial: ");
-		c.showFormatElements();
-		System.out.print("\nConjunto D con formato especial: ");
-		d.showFormatElements();
-		System.out.print("\nConjunto E con formato especial: ");
-		e.showFormatElements();
-		System.out.print("\nConjunto F con formato especial: ");
-		f.showFormatElements();
-	}
+	
 }
 
