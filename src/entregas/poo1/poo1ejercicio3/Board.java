e552616f509182c75e0ce897f540457226a58bea
selcox <sergio.munoz.lillo@gmail.com>
2020-02-27 10:45:23 +0100

    Fixed Img path

--- src/entregas/poo1/poo1ejercicio3/Board.java
+++ src/entregas/poo1/poo1ejercicio3/Board.java
@@ -6,8 +6,6 @@ import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Arrays;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
